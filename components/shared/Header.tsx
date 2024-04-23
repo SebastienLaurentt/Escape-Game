58fff5a6c4be25bf6d04f021e31410a609fa1e0d
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 00:19:53 +0200

    update content and css

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -5,8 +5,8 @@ import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between py-4 px-4">
-      <h1>Escape Room</h1>
+    <header className="flex flex-row justify-between items-center py-4 px-4 border-b-4 border-black">
+      <span className="font-bold text-xl">Escape Room</span>
       <div className="flex flex-row items-center gap-x-4">
         <SignedIn>
           <UserButton />
