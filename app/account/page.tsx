3a1945ed594be9273e57dae13405456f0833ae84
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-23 23:52:12 +0200

    update account with clerk user button allowing sign out action

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
 
 const account = () => {
   return (
-    <div>account</div>
-  )
-}
+    <div>
+      <div>
+        <UserButton />
+      </div>
+      <div>
+        <h1>Account</h1>
+      </div>
+    </div>
+  );
+};
 
-export default account
+export default account;
