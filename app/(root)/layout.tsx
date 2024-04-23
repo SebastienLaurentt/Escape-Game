8fa383fb5f2c100c091e97224a65e74f6a110166
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 00:24:19 +0200

    update css and import footer in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 
 export default function RootLayout({
@@ -6,9 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1" >{children}</main>
+      <Footer />
     </div>
 
   );
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='w-full px-4 mx-auto'>Footer</footer>
+    <footer className='w-full px-4 mx-auto border-t-4 border-black'>Footer</footer>
   )
 }
 
