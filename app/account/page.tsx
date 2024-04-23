66974d23c6b9a4b023510f7f57c25a8d0954e49d
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-23 23:59:22 +0200

    user button is now in header and also create Button to sign in in header

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,11 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
-
 const account = () => {
   return (
     <div>
-      <div>
-        <UserButton />
-      </div>
       <div>
         <h1>Account</h1>
       </div>
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { Button } from '../ui/button'
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 
 const Header = () => {
   return (
-    <header className='flex flex-row justify-between px-4'>
+    <header className="flex flex-row justify-between px-4">
       <h1>Escape Room</h1>
+      <div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Button>
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+      </div>
       <Button>Book Now</Button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
