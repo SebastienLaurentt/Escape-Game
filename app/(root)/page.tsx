6c3478217cfb879cca7056d22ed6c6c32fd1ba2e
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 00:45:25 +0200

    import hero component in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/shared/Hero";
+
 export default function Home() {
   return (
-    <main className="">
-      <h1>Escape Gamess </h1>
+    <main>
+      <Hero />
     </main>
   );
 }
