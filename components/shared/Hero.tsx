e971547eff22640bb1d6b01bf62eec09e8a6008f
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 00:56:58 +0200

    update structure

--- components/shared/Hero.tsx
+++ components/shared/Hero.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <h1>Bienvenue chez Escape Room</h1>
-  )
-}
+    <div className="flex flex-row justify-center">
+      <div className="flex flex-col ">
+        <h1 className="text-3xl font-bold  mt-12">
+          Bienvenue chez Escape Room !{" "}
+        </h1>
+        <span>
+          <p className="my-4">Réservez une salle pour une expérience unique</p>
+          <Button>Réserver</Button>
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
