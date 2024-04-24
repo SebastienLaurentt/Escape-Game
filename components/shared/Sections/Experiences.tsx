aeb83cc92ea34b39b088b8f4d32b619bcabb5a62
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 21:24:26 +0200

    create experience component

---
+++ components/shared/Sections/Experiences.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Section from '../Section'
+import SectionHeader from '../SectionHeader'
+
+const Experiences = () => {
+  return (
+    <Section marginBottom={true} marginTop={true} classname="">
+        <SectionHeader
+          title="Choissisez votre"
+          titleHighlight="expérience"
+        />
+    <ul>
+      <li>
+        
+      </li>
+    </ul>
+    </Section>
+  )
+}
+
+export default Experiences
