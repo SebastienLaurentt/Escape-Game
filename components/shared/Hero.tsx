db29d464be34addf34f622884bdf372a7035c662
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 11:57:57 +0200

    add homeBg

--- components/shared/Hero.tsx
+++ components/shared/Hero.tsx
@@ -1,15 +1,18 @@
+import Image from "next/image";
+import BgHome from "../../public/images/BgHome.webp";
 import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
     <div className="flex flex-row justify-center">
-      <div className="flex flex-col ">
+      <Image src={BgHome} width={1920} height={1080} alt="home picture" className="relative " />
+      <div className="absolute text-white flex flex-col ">
         <h1 className="text-3xl font-bold  mt-12">
           Bienvenue chez Escape Room !{" "}
         </h1>
         <span>
           <p className="my-4">Réservez une salle pour une expérience unique</p>
-          <Button>Réserver</Button>
+          <Button className="bg-white text-black">Réserver</Button>
         </span>
       </div>
     </div>
