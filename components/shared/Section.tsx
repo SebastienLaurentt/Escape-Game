981696086997edb7bcadd7d08a30f82fe3a162ca
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 19:19:13 +0200

    create section component

---
+++ components/shared/Section.tsx
@@ -0,0 +1,26 @@
+interface SectionProps {
+  classname?: string;
+  marginBottom?: boolean;
+  marginTop?: boolean;
+  children: React.ReactNode;
+}
+
+const Section = ({
+  classname,
+  marginBottom,
+  marginTop,
+  children,
+}: SectionProps) => {
+  const isMarginBottom = marginBottom ? "mb-32 md:mb-36 xl:mb-48" : "";
+  const isMarginTop = marginTop ? "mt-20 md:mt-24 xl:mt-28" : "";
+
+  return (
+    <section
+      className={`${classname} ${isMarginBottom} ${isMarginTop} px-6 md:px-10 xl:mx-auto xl:max-w-screen-xl xl:px-16`}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default Section;
