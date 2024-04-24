7b2b789965b4df5180fb59ef53e55a89e1ae6633
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 22:17:04 +0200

    add px and max w from md display

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full text-xs md:text-sm px-4 py-4 mx-auto  flex flex-row justify-between ">
+    <footer className="w-full text-xs md:text-sm px-4 py-4 mx-auto  flex flex-row justify-between md:px-6 lg:px-10 xl:px-16 xl:max-w-[1400px] xl:mx-auto ">
       <span>© 2021 Escape Room</span>
       <div>
         <span>Mentions Légales</span>
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between items-center py-4 px-4">
+    <header className="flex flex-row justify-between items-center py-4 px-4 md:px-6 lg:px-10 xl:px-16 xl:w-[1400px] mx-auto">
       <span className="font-bold text-md md:text-xl">Escape Room</span>
       <div className="hidden md:flex flex-row items-center gap-x-4">
         <SignedIn>
