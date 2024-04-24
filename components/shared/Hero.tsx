42bfc1b571ac2126f7ac5e84af85aee56eb1a31e
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 14:35:02 +0200

    update hero css (eg black gardient only for img)

--- components/shared/Hero.tsx
+++ components/shared/Hero.tsx
@@ -4,16 +4,23 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <div className="flex flex-row justify-center h-screen ">
-      <Image src={BgHome} alt="home picture" className="relative object-cover " />
-      <div className="absolute text-white flex flex-col ">
-        <h1 className="text-3xl font-bold  mt-12">
+    <div className="relative flex flex-row justify-center h-screen overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-transparent to-black opacity-100 z-10" />
+      <div className="relative">
+        <Image
+          src={BgHome}
+          alt="home picture"
+          className="object-cover"
+        />
+      </div>
+      <div className="absolute text-white flex flex-col items-center px-4 mt-12 z-20">
+        <h1 className="text-3xl text-center font-bold">
           Bienvenue chez Escape Room !{" "}
         </h1>
-        <span>
-          <p className="my-4">Réservez une salle pour une expérience unique</p>
-          <Button className="bg-white text-black">Réserver</Button>
-        </span>
+        <p className="my-4 w-[200px] md:text-lg md:w-[400px] text-center">
+          Réservez une salle pour une expérience unique
+        </p>
+        <Button className="bg-white text-black">Réserver</Button>
       </div>
     </div>
   );
