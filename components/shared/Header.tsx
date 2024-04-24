a3afdc170074d70069a0fbd1871e9df3c4f03788
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 23:01:38 +0200

    header is absolute in home

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,25 +1,34 @@
+"use client";
+
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import logo from "../../public/images/Logo.svg";
 import { Button } from "../ui/button";
 
 const Header = () => {
+  const pathname = usePathname();
+  const headerPosition = pathname === "/" ? "absolute top-0 z-20" : "";
   return (
-    <header className="flex flex-row justify-between items-center py-4 px-4 md:px-6 lg:px-10 xl:px-16 xl:w-[1400px] xl:mx-auto">
-      <Link href="/">
-        <Image src={logo} alt="Escape Room" width={50} height={50} />
-      </Link>
-      <div className="hidden md:flex flex-row items-center gap-x-4">
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedOut>
-          <Button>
-            <Link href="/sign-in">Se connecter </Link>
-          </Button>
-        </SignedOut>
-        <Button>Réserver</Button>
+    <header
+      className={`${headerPosition} w-full `}
+    >
+      <div className="flex  items-center  xl:mx-auto  flex-row justify-between xl:px-16 xl:max-w-[2000px] py-4">
+        <Link href="/">
+          <Image src={logo} alt="Escape Room" width={50} height={50} />
+        </Link>
+        <div className="hidden md:flex flex-row items-center gap-x-4">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <Button>
+              <Link href="/sign-in">Se connecter </Link>
+            </Button>
+          </SignedOut>
+          <Button>Réserver</Button>
+        </div>
       </div>
     </header>
   );
