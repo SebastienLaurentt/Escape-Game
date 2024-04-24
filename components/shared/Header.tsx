bc1c29cd801150800271092f6a88a1ea1e3718fe
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 22:52:29 +0200

    add logo and nested him in home link

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,12 +1,15 @@
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
+import logo from "../../public/images/Logo.svg";
 import { Button } from "../ui/button";
-
 
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between items-center py-4 px-4 md:px-6 lg:px-10 xl:px-16 xl:w-[1400px] mx-auto">
-      <span className="font-bold text-md md:text-xl">Escape Room</span>
+    <header className="flex flex-row justify-between items-center py-4 px-4 md:px-6 lg:px-10 xl:px-16 xl:w-[1400px] xl:mx-auto">
+      <Link href="/">
+        <Image src={logo} alt="Escape Room" width={50} height={50} />
+      </Link>
       <div className="hidden md:flex flex-row items-center gap-x-4">
         <SignedIn>
           <UserButton />
