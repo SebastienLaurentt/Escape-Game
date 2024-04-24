5446f02122eaa02d298a0d43c768684cea89ef91
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 14:43:42 +0200

    update css

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full px-4 py-3 mx-auto bg-black text-white  flex flex-row justify-between ">
+    <footer className="w-full text-xs md:text-sm px-4 py-4 mx-auto bg-black text-white  flex flex-row justify-between ">
       <span>© 2021 Escape Room</span>
       <div>
         <span>Mentions Légales</span>
