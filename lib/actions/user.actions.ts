22f32f0f56e5dc458518684139ba103c2e4156ef
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 15:13:38 +0200

    import handle error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,11 +1,12 @@
 'use server'
 
 import  { CreateUserParams } from '@/types'
+import { handleError } from '../utils'
 
 export const createUser = async (user: CreateUserParams) => {
   try {
 
   } catch (error) {
-    console.error(error)
+    handleError(error)
   }
 }
