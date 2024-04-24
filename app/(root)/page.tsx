ccf1fa5adc0b5741bb0690d289b4484e6d69d8c9
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 19:18:18 +0200

    import info section in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/shared/Sections/Hero";
+import Infos from "@/components/shared/Sections/Infos";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Infos />
     </main>
   );
 }
