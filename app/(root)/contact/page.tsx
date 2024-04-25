117f18db829eecf3d063f2ed3bf942471c7da1e1
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-25 16:15:32 +0200

    import pageTitle

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,10 +1,15 @@
-import Infos from '@/components/shared/Sections/Infos'
-import React from 'react'
+import PageTitle from "@/components/shared/PageTitle";
+import Infos from "@/components/shared/Sections/Infos";
 
 const Contact = () => {
   return (
-    <Infos  />
-  )
-}
+    <main>
+      <PageTitle
+        title="Contact"
+      />
+      <Infos />
+    </main>
+  );
+};
 
-export default Contact
+export default Contact;
