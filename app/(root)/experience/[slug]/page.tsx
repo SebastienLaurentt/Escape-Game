b0bf1a2bdeca40621cb24d48cb2a60fc7c5610f7
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-25 13:17:52 +0200

    update content

--- app/(root)/experience/[slug]/page.tsx
+++ app/(root)/experience/[slug]/page.tsx
@@ -1,14 +1,29 @@
 import Section from '@/components/shared/Section';
 import React from 'react'
+import experienceData from "@/data/experienceData";
+
+
+type ExperienceData = {
+  name: string;
+  description: string;
+};
 
 const Experience = ({
   params,
 }: {
   params: { slug: string };
 }) => {
+
+  const slug = params.slug;
+  const experience = experienceData[
+    slug as keyof typeof experienceData
+  ] as ExperienceData;
+
+
   return (
     <Section marginBottom={true} marginTop={true} classname="">
-      <h1>{params.slug}</h1>
+      <h1>{experience.name}</h1>
+      <span>{experience.description}</span>
     </Section>
   )
 }
--- data/experienceData.ts
+++ data/experienceData.ts
@@ -1,16 +1,16 @@
 
 
 const experienceData = {
-  experience1: {
+  horror: {
     name: "Horror",
     description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
-  experience2: {
-    name: "Sandamal",
+  thriller: {
+    name: "Thriller",
     description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
-  experience3: {
-    name: "Galopins",
+  night: {
+    name: "Night",
     description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     // images: [
     //   { src: GalopinsLarge1, description: "Page Parcours" },
