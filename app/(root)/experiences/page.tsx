6d497a958687588ec702c842e3a1f021004c428e
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-25 20:25:59 +0200

    import ExperienceLongerCard in experiences page

--- app/(root)/experiences/page.tsx
+++ app/(root)/experiences/page.tsx
@@ -1,10 +1,51 @@
-import PageTitle from '@/components/shared/PageTitle'
-import React from 'react'
+import ExperienceLongerCard from "@/components/shared/ExperienceLongerCard";
+import PageTitle from "@/components/shared/PageTitle";
+import Section from "@/components/shared/Section";
+import HorrorImg from "../../../public/images/Experience1.jpg";
 
 const Experiences = () => {
   return (
-    <PageTitle title="Choisissez votre expérience" />
-  )
-}
+    <>
+      <PageTitle title="Choisissez votre expérience" />
+      <Section marginBottom={true} marginTop={true} classname="">
+        <ul className="flex flex-col lg:flex-row lg:gap-x-4">
+          <li>
+            <ExperienceLongerCard
+              src={HorrorImg}
+              alt="Logo de l'expérience Horror"
+              name="Horror"
+              description="lorem00000"
+              peopleNumber="4-6"
+              duration="1"
+              price="40"
+            />
+          </li>
+          <li>
+            <ExperienceLongerCard
+              src={HorrorImg}
+              alt="Logo de l'expérience Horror"
+              name="Horror"
+              description="lorem00000"
+              peopleNumber="4-6"
+              duration="1"
+              price="40"
+            />
+          </li>
+          <li>
+            <ExperienceLongerCard
+              src={HorrorImg}
+              alt="Logo de l'expérience Horror"
+              name="Horror"
+              description="lorem00000"
+              peopleNumber="4-6"
+              duration="1"
+              price="40"
+            />
+          </li>
+        </ul>
+      </Section>
+    </>
+  );
+};
 
-export default Experiences
+export default Experiences;
