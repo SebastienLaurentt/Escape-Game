167081e73016e189a8353826cecc49c3e9fdf92a
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-25 13:14:05 +0200

    create experience data

---
+++ data/experienceData.ts
@@ -0,0 +1,23 @@
+
+
+const experienceData = {
+  experience1: {
+    name: "Horror",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  experience2: {
+    name: "Sandamal",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  experience3: {
+    name: "Galopins",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    // images: [
+    //   { src: GalopinsLarge1, description: "Page Parcours" },
+    //   { src: GalopinsLarge2, description: "Page Club" },
+    //   { src: GalopinsLarge3, description: "Contact" }
+    // ],
+  },
+};
+
+export default experienceData;
