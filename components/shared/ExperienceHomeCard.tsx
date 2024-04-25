a1acbd334b464556f73f903cdd9d74628536ff4b
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-25 16:42:54 +0200

    create ExperienceHomeCard

--- components/shared/ExperienceHomeCard.tsx
+++ components/shared/ExperienceHomeCard.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 
-const ExperienceHomeCard = () => {
-  return (
-    <div>ExperienceHomeCard</div>
-  )
+interface ExperienceHomeCardProps {
+  href: string;
+  src: StaticImageData;
+  alt: string;
+  name?: string;
 }
 
-export default ExperienceHomeCard
+const ExperienceHomeCard = ({
+  href,
+  src,
+  alt,
+  name,
+}: ExperienceHomeCardProps) => {
+  return (
+    <Link
+      href={href}
+      className="relative mx-auto text-white xl:text-white/0 xl:hover:text-white"
+    >
+      <Image
+        src={src}
+        alt={alt}
+        className="rounded-xl shadow-md shadow-gray-600"
+      />
+      <span className=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl font-bold uppercase">
+        {name}
+      </span>
+    </Link>
+  );
+};
+
+export default ExperienceHomeCard;
