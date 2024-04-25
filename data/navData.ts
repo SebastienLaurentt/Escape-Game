48b837bd4a6fbf23215babdf1e33f5715f5d08d8
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-25 12:20:30 +0200

    create navData

---
+++ data/navData.ts
@@ -0,0 +1,19 @@
+const navData = [
+  {
+    href: "/",
+    name: "Accueil",
+    burgerMenuName: "ACCUEIL",
+  },
+  {
+    href: "/",
+    name: "Expériences",
+    burgerMenuName: "EXPERIENCES",
+  },
+  {
+    href: "/",
+    name: "Contact",
+    burgerMenuName: "CONTACT",
+  },
+];
+
+export default navData;
