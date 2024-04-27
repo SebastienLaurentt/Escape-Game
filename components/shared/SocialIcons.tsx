abbb74ea5e1b195e54d301a1869361b0a370978b
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-27 21:17:16 +0200

    create socialIcons component

---
+++ components/shared/SocialIcons.tsx
@@ -0,0 +1,20 @@
+import { Facebook, Instagram, Link } from "lucide-react";
+
+const SocialIcons = ({ id }: { id: string }) => {
+  return (
+    <div id={id} className="flex flex-row justify-center gap-x-8">
+      <button aria-label="Allez à la page Instagram de Escape Room">
+        <Link href="" target="_blank">
+          <Instagram className="  size-8 md:size-10 lg:size-12" />
+        </Link>
+      </button>
+      <button aria-label="Allez à la page Facebook de Escape Room">
+        <Link href="" target="_blank">
+          <Facebook className="  size-8 md:size-10 lg:size-12" />
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default SocialIcons;
