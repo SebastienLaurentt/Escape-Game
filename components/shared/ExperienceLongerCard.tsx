c25a107a4f00f45cdf2b37c4753fe3814d8ef382
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-29 00:40:01 +0200

    update content and css

--- components/shared/ExperienceLongerCard.tsx
+++ components/shared/ExperienceLongerCard.tsx
@@ -1,4 +1,4 @@
-import { Euro, Hourglass, User } from "lucide-react";
+import { Hourglass, User } from "lucide-react";
 import Image, { StaticImageData } from "next/image";
 
 interface ExperienceLongerCardProps {
@@ -32,27 +32,25 @@ const ExperienceLongerCard = ({
       onClick={onClick}
     >
       <Image src={src} alt={alt} className="rounded-xl" />
-      <div className="flex flex-col gap-y-2 p-3">
+      <div className="flex flex-col p-4">
         <h2>{name}</h2>
-        <span>{description}</span>
-        <div className="flex flex-row items-center gap-x-1">
-          <span className="text-xl">{peopleNumber}</span>
-          <User size={40} />
-        </div>
-        <div className="flex flex-row items-center gap-x-1">
-          <span className="text-xl">{duration}</span>
-          <Hourglass size={40} />
-        </div>
-        <div className="flex flex-row items-center justify-between gap-x-1">
-          <div className="flex flex-row">
-            <span className="text-xl">{price}</span>
-            <Euro size={40} />
+        <span className="mb-4">{description}</span>
+        <div className="flex flex-row gap-x-12">
+          <div className="flex flex-row items-center gap-x-1">
+            <User size={32} />
+            <span className="text-md">{peopleNumber}</span>
           </div>
-          {isSelected ? (
-            <span className="rounded-xl bg-accent px-2 py-1">
-              Votre Sélection
-            </span>
-          ) : null}
+          <div className="flex flex-row items-center gap-x-1">
+            <Hourglass size={32} />
+            <span className="text-md">{duration}</span>
+          </div>
+          <div className="flex flex-row items-center justify-between gap-x-1">
+            {isSelected ? (
+              <span className="rounded-xl bg-accent px-2 py-1">
+                Votre Sélection
+              </span>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
