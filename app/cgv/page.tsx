b7eff2543be7ee69a6fcd026831cabe0a7104529
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-28 21:56:41 +0200

    create cgv page

---
+++ app/cgv/page.tsx
@@ -0,0 +1,295 @@
+import PageTitle from "@/components/shared/PageTitle";
+import Section from "@/components/shared/Section";
+
+const CGV = () => {
+  return (
+    <main>
+      <Section marginBottom={true} marginTop={true}>
+        <PageTitle title="Conditions Générales de Ventes" />
+        <div className="flex flex-col gap-y-12 text-balance text-sm md:text-md">
+          <div>
+            <h3>ARTICLE 1 – APPLICATION ET CONCEPT</h3>
+            <p>
+              XXXX est un concept de jeu d’évasion grandeur nature
+              appartenant à la société XXX. Les présentes conditions
+              générales de vente sont applicables à tout contrat de mise à
+              disposition d’une salle de jeu conclu entre (1) toute société du
+              groupe XXX XXX et (2) les utilisateurs de la salle,
+              lesquels seront nommés « UTILISATEURS » ensuite ; qu’ils aient
+              réservé personnellement ou qu’ils fassent partie de l’équipe de la
+              personne ayant effectuée la réservation, dénommée ci-après «
+              L’ACHETEUR ». Ces conditions s’appliquent également quel que soit
+              le moyen de réservation, que celle-ci soit effectuée via le site
+              internet de XXX XXX, nommé ci-après « SITE INTERNET » ou
+              auprès du personnel de la société XXX XXX. L’espace de jeu
+              mis à la disposition des joueurs est dénommé « SALLE » et le
+              contrat de mise à disposition « CONTRAT ». La partie de jeu pour
+              laquelle l’ACHETEUR réserve et paie sur le SITE INTERNET est
+              appelée « SESSION DE JEU ». XXX XXX reconnaît le droit de
+              modifier ou faire modifier tout ou partie des présentes conditions
+              générales de vente ainsi que le contenu de son SITE INTERNET
+              (XXXXXX XXXXX), ceci à sa seule discrétion, à tout
+              moment et sans préavis. Les conditions générales de vente de la
+              société XXX XXX sont disponibles sur le SITE INTERNET et
+              peuvent être consultées librement sur simple demande auprès du
+              personnel d’accueil de la société XXX XXX.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 2 : RÉSERVATION</h3>
+            <p>
+              L’accès aux installations et services de XXX XXX
+              s’effectue exclusivement après réservation soit sur le SITE
+              INTERNET XXXX XXX ou auprès du personnel de la
+              société XXX XXX. La réservation est effectuée pour un jour
+              et un horaire précis, et n’est valable que pour ce créneau
+              horaire. La réservation s’effectue dans la limite des places
+              disponibles, qui sont actualisées en temps réel sur le site
+              internet. La réservation ne sera ferme et définitive qu’après
+              réception par le client d’un e-mail de confirmation de XXX
+              XXX. Vous êtes responsable de la saisie exacte de vos
+              coordonnées dans le formulaire de réservation en ligne. En
+              soumettant ce dernier vous garantissez à XXX XXX que les
+              détails fournis sont véridiques et exacts. XXX XXX ne
+              saurait être tenu responsable pour quelconque erreur de saisie ou
+              erreur de choix de date et horaire de votre part. Les réservations
+              peuvent être reportées jusqu’à 7 jours avant la date de la SESSION
+              DE JEU en fonction des disponibilités sur demande écrite à
+              XXX XXXX . En dessous de 7 jours, aucun changement,
+              ni annulation ou report ne sont possibles. En aucun cas UNE
+              SESSION DE JEU n’est remboursable.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 3 – MISE À DISPOSITION ET UTILISATION DE LA SALLE</h3>
+            <p>
+              Selon le principe même du jeu, XXX XXX accorde aux
+              UTILISATEURS le droit d’occuper la SALLE pour une durée maximale
+              de 60 (soixante) minutes. L’occupation de la SALLE a pour seul et
+              unique but de participer à un jeu organisé par la société XXX
+              XXX, lequel doit être réalisé en équipe de 2 à 7 personnes
+              maximum selon la mission réservée. Les UTILISATEURS acceptent que
+              la durée de présence dans la SALLE soit de 60 minutes maximum,
+              ceci qu’ils soient parvenus à trouver tous les indices et à
+              résoudre toutes les énigmes ou non. Ce point relève du principe
+              même du jeu. Parallèlement, les UTILISATEURS acceptent que le jeu
+              s’arrête avant la fin des 60 minutes, notamment s’ils sont
+              parvenus à trouver tous les indices et à résoudre toutes les
+              énigmes en moins de 60 minutes ; sans que cela fasse l’objet d’un
+              quelconque ajustement ou compensation tarifaire. L’admission dans
+              la SALLE n’est possible que sur présentation de l’email de
+              confirmation de réservation (envoyé peu après le processus de
+              paiement à l’ACHETEUR) et sur présentation d’une pièce d’identité
+              de ce dernier. La complexité du jeu ne permet pas aux UTILISATEURS
+              dont l’âge est inférieur à 8 ans de participer aux missions LE
+              SORCIER MAUDIT et MECANISATION. Pour chaque SESSION DE JEU, 1
+              adulte est nécessaire. En cas de doute sur l’âge d’un UTILISTEUR,
+              la société XXX XXX se réserve le droit de demander un
+              justificatif d’identité. Les UTILISATEURS sont tenus de maintenir
+              la SALLE et l’intégralité de ses équipement (liste non restrictive
+              : décorations, ameublement, mécanismes, etc.) en bon état et de
+              l’utiliser conformément à l’usage autorisé. De ce fait,
+              l’UTILISATEUR reconnaît au CONTRAT que toute dégradation
+              intentionnelle ou causée par une manipulation non autorisée de la
+              salle et de ses équipements fera l’objet d’une refacturation. Par
+              ailleurs, l’UTILISATEUR est tenu d’utiliser la SALLE et ses
+              équipements de sorte à n’occasionner ou risquer d’occasionner
+              aucun dommage aux autres UTILISATEURS, aux locaux et matériels de
+              XXX XXX. Durant toute sa présence au sein des locaux de
+              XXX XXX, l’UTILISATEUR s’engage à se comporter de façon
+              respectable, à se conformer aux règles et à coopérer avec le
+              personnel de XXX XXX. À ce titre, XXX XXX ne
+              tolère aucune forme de violence, qu’elle soit verbale ou physique,
+              sous forme de harcèlement ou d’intimidation envers ses employés
+              et/ou les autres UTILISATEURS et se réserve ainsi le droit de
+              refuser l’accès aux locaux à toute personne dont le comportement
+              s’apparenterait aux éléments cités précédemment. XXX XXX
+              se réserve également le droit de refuser l’accès à toute personne
+              sous l’influence de l’alcool ou de drogues et ce par mesure de
+              sécurité pour ladite personne et son entourage. Pour toutes les
+              conditions mentionnées ci-dessus, l’interdiction d’accès ne donne
+              droit à aucune indemnité ou remboursement. En raison de la nature
+              du jeu, aucun enregistrement ne peut être effectué dans la SALLE.
+              Ainsi, l’utilisation d’appareils d’enregistrement vidéo et/ou
+              audio (cela inclut notamment et sans s’y limiter : téléphones
+              mobiles, caméras, Go Pro) est formellement interdite. Toute
+              utilisation d’appareil de ce type pourra occasionner l’arrêt
+              complet du jeu et ce pour toute l’équipe. XXX XXX attend
+              de ses UTILISATEURS qu’ils ne divulguent pas les détails du jeu de
+              façon directe et/ou indirecte. Toute divulgation partielle et
+              totale violerait les intérêts commerciaux de la société XXX
+              XXX.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 4 – PRIX</h3>
+            <p>
+              Les tarifs indiqués par XXX XXX sont en euros, toutes
+              taxes comprises, tenant compte de la TVA applicable au jour de la
+              commande, tout changement du taux pouvant être répercuté sur le
+              prix des services. Les prix pratiqués par XXX XXX sont
+              indiqués sur le SITE INTERNET. Toute autre prestation donnera lieu
+              à une facturation additionnelle. XXX XXX se réserve le
+              droit de modifier ses tarifs à tout moment. Tout ACHETEUR paiera
+              le montant relatif au tarif en vigueur au moment de sa
+              réservation. En cas de retard des UTILISATEURS pour une SESSION
+              réservée, l’UTILISATEUR pourra décider s’il souhaite ou non
+              débuter la partie. Dans tous les cas, la SALLE devra être libérée
+              à l’heure prévue dans la réservation initiale. XXX XXX se
+              réserve le droit de refuser l’accès aux UTILISATEURS dont le
+              retard s’élève à 15 minutes ou plus. Les horaires seront
+              systématiquement rappelés dans les emails de confirmation envoyés
+              par XXX XXX. Si moins d’UTILISATEURS se présentent pour
+              participer à la SESSION de jeu que ce qui était initialement prévu
+              lors de la réservation, l’ACHETEUR et/ou les UTILISATEURS ne
+              peuvent prétendre au remboursement de la différence de prix. Si
+              plus d’UTILISATEURS se présentent pour participer à la SESSION de
+              jeu que ce qui était initialement prévu lors de la réservation, la
+              différence de prix devra être réglée avant de participer au jeu,
+              ceci dans la limite du nombre d’UTILISATEURS autorisés par SALLE.
+              XXX XXX se réserve le droit de refuser toute réservation à
+              un ACHETEUR qui n’aurait pas payé l’ensemble des prestations qui
+              lui auraient été facturées.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 5 – ENTREPRISES</h3>
+            <p>
+              Cet article concerne les entreprises ayant réservé hors site
+              internet. Les prix pratiqués par XXX XXX sont indiqués sur
+              le devis et varient en fonction de la prestation convenue
+              contractuellement avec l’ACHETEUR. Le règlement peut être effectué
+              par L’ACHETEUR sous forme de chèque ou virement bancaire. Un
+              acompte de 50% doit être réglé à la commande afin de garantir la
+              réservation. Le solde est dû au plus tard le jour de l’événement.
+              En cas d’absence totale de règlement 7 jours avant la prestation,
+              XXX XXX se réserve le droit d’annuler la commande. Le
+              nombre de missions pourra être révisé à la baisse (dans la limite
+              d’une mission) jusqu’à 7 jours avant la date de la prestation. Une
+              fois la commande effectuée par L’ACHETEUR (matérialisée par la
+              signature d’un devis signé avec la mention bon pour accord), les
+              conditions d’annulation suivantes s’appliquent : Annulation à 15
+              jours ou plus avant la date de la prestation : Si le règlement de
+              la prestation ou de l’acompte de 50% a déjà été effectué par
+              l’ACHETEUR, aucun remboursement ne sera effectué par XXX
+              XXX. Si le règlement n’a pas encore été effectué par
+              l’ACHETEUR, une pénalité correspondant à 50% du prix TTC de la
+              prestation sera exigée par XXX XXX. XXX XXX pourra
+              proposer à l’ACHETEUR une nouvelle date pour reprogrammer sa
+              SESSION DE JEU. Annulation entre 15 et 7 jours avant la date de la
+              prestation : Si le règlement de la prestation ou de l’acompte de
+              50% a déjà été effectué par l’ACHETEUR, aucun remboursement ne
+              sera effectué par XXX XXX. Si le règlement n’a pas encore
+              été effectué par l’ACHETEUR, une pénalité correspondant à 50% du
+              prix TTC de la prestation sera exigée par XXX XXX. Aucun
+              report de date ne pourra être proposé à l’ACHETEUR. Annulation à
+              moins de 7 jours avant la date de la prestation : Si le règlement
+              de la prestation ou de l’acompte de 50% a déjà été effectué par
+              l’ACHETEUR, aucun remboursement ne sera effectué par XXX
+              XXX. Si le règlement n’a pas encore été effectué par
+              l’ACHETEUR, une pénalité correspondant à 100% du prix TTC de la
+              prestation sera exigée par XXX XXX. Aucun report de date
+              ne pourra être proposé à l’ACHETEUR.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 6 – DROIT DE RÉTRACTATION</h3>
+            <p>
+              Conformément à l’article 121-21-8 du code de la consommation, le
+              droit de rétractation ne peut être exercé pour les contrats de
+              prestations de services d’hébergement, autres que d’hébergement
+              résidentiel, de services de transport de biens, de locations de
+              voitures, de restauration ou d’activités de loisirs qui doivent
+              être fournis à une date ou à une période déterminée. Par
+              conséquent, aucune réservation ne peut être annulée ou remboursée
+              sauf dans le cadre d’une annulation du seul fait de XXX
+              XXX. Dans ce dernier cas, le montant de la réservation
+              initiale sera remboursée
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 7 – DONNÉES PERSONNELLES</h3>
+            <p>
+              L’ACHETEUR est responsable de l’exactitude des informations
+              saisies sur le formulaire de réservation en ligne sur le SITE
+              INTERNET. L’ACHETEUR accepte de recevoir de la part de XXX
+              XXX des informations et offres commerciales sur son adresse
+              email ou sur son téléphone mobile. L’ACHETEUR et/ou l’UTILISATEUR
+              qui saisit ses coordonnées téléphoniques sur le SITE INTERNET de
+              XXX XXX accepte d’ores et déjà d’être contacté par XXX
+              XXX par téléphone ou SMS. L’ACHETEUR renseignant les
+              coordonnées des autres joueurs / UTILISATEURS (noms et prénoms,
+              numéros de téléphone, adresses email) autorise XXX XXX à
+              envoyer des informations et offres commerciales à ces derniers. A
+              XXX XXX s’engage à ne pas transmettre les données
+              personnelles de ses UTILISATEURS à une société extérieure au
+              groupe XXX XXX, hors requête d’une autorité habilitée par
+              la loi et dans le cadre des dispositions légales en vigueur.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 8 – SÉCURITÉ</h3>
+            <p>
+              L’UTILISATEUR s’engage à respecter toutes les mesures de sécurité
+              qui lui sont données, qu’elles soient contractuelles, affichées
+              sur le site ou données oralement par le personnel de XXX
+              XXX. Il est formellement et strictement interdit de fumer à
+              l’intérieur du site. Chaque UTILISATEUR s’engage à prendre
+              connaissance et respecter les consignes de sécurité et protection
+              incendie, de même que les issues de secours. Il est à ce titre
+              formellement interdit de gêner les issues de secours, lesquelles
+              doivent rester dégagées en toute circonstance. L’UTILISATEUR
+              pourra utiliser les issues de secours si et seulement si la
+              situation exige l’évacuation du local. Tout abus et/ou ouverture
+              intempestive de ces issues par un ou plusieurs UTILISATEUR(S)
+              entraînera la refacturation des coûts occasionnés par cet abus.
+              L’UTILISATEUR est au fait qu’il est absolument interdit d’apporter
+              dans les locaux et dans la SALLE des objets considérés comme (1)
+              des dangers physiques (explosif, inflammable, comburant, gaz sous
+              pression ou liquéfié, corrosif), (2) dangereux pour la santé
+              (toxique, corrosif pour la peau, irritant/sensibilisant,
+              cancérogène/tératogène) et (3) dangereux pour le milieu aquatique.
+              De même, l’usage de la force et/ou de la violence est formellement
+              interdit. Toute dégradation de la SALLE due à une utilisation
+              anormale de l’espace jeu pourra donner lieu à une refacturation.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 9 – PHOTOS ET CAMÉRAS</h3>
+            <p>
+              Des photos de chaque équipe d’UTILISATEURS pourront être prises
+              avant ou après la session de jeu. Ces photos pourront être
+              utilisées par XXX XXX afin d’alimenter son site internet
+              ou son écosystème digital (Exemples sans restriction : Facebook,
+              Twitter, Instagram etc.). XXX XXX ne pourra être tenu
+              responsable de l’utilisation qui sera faite des photos par les
+              UTILISATEURS et / ou l’ACHETEUR. Chaque SALLE comporte un
+              dispositif de vidéosurveillance et d’écoute permettant au
+              personnel de XXX XXX de suivre la session de jeu et
+              d’aiguiller les UTILISATEURS si besoin.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 10 – PROPRIÉTÉ</h3>
+            <p>
+              L’ensemble des éléments, textes, vides, images du site
+              www.lyon-escape-game.fr sont la propriété exclusive de XXX
+              XXX. Toute reproduction est strictement interdite.
+            </p>
+          </div>
+          <div>
+            <h3>ARTICLE 11 – TRIBUNAUX COMPÉTENTS</h3>
+            <p>
+              Les présentes conditions générales et tout contrat conclu
+              incorporant ces termes et conditions sont régis par le droit
+              français et tous les litiges sont soumis à la compétence exclusive
+              des tribunaux français.
+            </p>
+          </div>
+        </div>
+      </Section>
+    </main>
+  );
+};
+
+export default CGV;
