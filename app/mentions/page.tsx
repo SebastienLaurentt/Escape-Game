acbee288e995f250c344dda0a2022a536d3e8f74
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-28 21:41:08 +0200

    create mentions page

---
+++ app/mentions/page.tsx
@@ -0,0 +1,115 @@
+import PageTitle from "@/components/shared/PageTitle";
+import Section from "@/components/shared/Section";
+
+const page = () => {
+  return (
+    <main>
+      <Section marginBottom={true} marginTop={true}>
+        <PageTitle title="Mentions légales" />
+        <div className="flex flex-col gap-y-4 text-balance text-sm md:text-md">
+          <p>
+            Le présent site est la propriété de la société TEST, SAS au capital
+            de 100€ immatriculée sous le numéro XXX XXX XXX au RCS de LYON, dont
+            le siège social est situé au XXX, XXXXXXXXXX. L’activité de la
+            société est exercée à la même adresse.
+          </p>
+          <p>
+            SIRET : XXXXXX| NAF : XXXX | N° de TVA : XXXXXX Tél : 01 23 45 67 89
+            | Email : XXXX |
+          </p>
+          <p>
+            L&apos;utilisateur du site XXXXXXXX reconnaît disposer de la
+            compétence et des moyens nécessaires pour accéder et utiliser ce
+            site. Il reconnaît également avoir vérifié que la configuration
+            informatique utilisée ne contient aucun virus et qu&apos;elle est en
+            parfait état de fonctionnement. Enfin, l&apos;utilisateur reconnaît
+            également avoir pris connaissance de la présente notice légale et
+            s&apos;engage à la respecter.
+          </p>
+          <p>
+            L&apos;utilisateur est informé que lors de ses visites sur le site,
+            un cookie peut s&apos;installer automatiquement sur son logiciel de
+            navigation. Un cookie est un élément qui ne permet pas
+            d&apos;identifier l&apos;utilisateur mais sert à enregistrer des
+            informations relatives à la navigation de celui-ci sur le site
+            Internet. Le paramétrage de votre logiciel de navigation permet
+            d&apos;informer de la présence de cookies et éventuellement de la
+            refuser selon la procédure décrite à l&apos;adresse suivante :
+            www.cnil.fr.
+          </p>
+          <p>
+            Ce site utilise Google Analytics, un service d&apos;analyse de site
+            Internet fourni par Google Inc. (« Google »). Google Analytics
+            utilise des cookies, qui sont des fichiers texte placés sur votre
+            ordinateur, pour aider le site internet à analyser
+            l&apos;utilisation du site par ses utilisateurs. Les données
+            générées par les cookies concernant votre utilisation du site (y
+            compris votre adresse IP) seront transmises et stockées par Google
+            sur des serveurs situés aux Etats-Unis. Google utilisera cette
+            information dans le but d&apos;évaluer votre utilisation du site, de
+            compiler des rapports sur l&apos;activité du site à destination de
+            son éditeur et de fournir d&apos;autres services relatifs à
+            l&apos;activité du site et à l&apos;utilisation d&apos;Internet.
+            Google est susceptible de communiquer ces données à des tiers en cas
+            d&apos;obligation légale ou lorsque ces tiers traitent ces données
+            pour le compte de Google, y compris notamment l&apos;éditeur de ce
+            site. Google ne recoupera pas votre adresse IP avec toute autre
+            donnée détenue par Google. Vous pouvez désactiver l&apos;utilisation
+            de cookies en sélectionnant les paramètres appropriés de votre
+            navigateur. Cependant, une telle désactivation pourrait empêcher
+            l&apos;utilisation de certaines fonctionnalités de ce site. En
+            utilisant ce site Internet, vous consentez expressément au
+            traitement de vos données nominatives par Google dans les conditions
+            et pour les finalités décrites ci-dessus.
+          </p>
+          <p>
+            {" "}
+            L&pos;utilisateur dispose d&pos;un droit d&pos;accès, de
+            modification, de rectification et de suppression des données qui le
+            concernent (article 38 et suivants de la loi &pos; Informatique et
+            Libertés &pos;). Pour l&pos;exercer, l&pos;utilisateur peut
+            s&pos;adresser à XXXXXXXXXXX XXXXXXXX
+          </p>
+          <p>
+            {" "}
+            Ce site est la propriété exclusive de XXX XXXXX. Toute reproduction
+            ou représentation totale ou partielle de ce site par quelque procédé
+            que ce soit, sans l&pos;autorisation expresse de SAS XXX XXXX est
+            interdite et constituerait une contrefaçon sanctionnée par les
+            articles L 335-2 et suivants du Code de la propriété intellectuelle.
+          </p>
+          <p>
+            Les bases de données figurant sur le site Internet sont protégées
+            par les dispositions de la loi du 11 juillet 1998 portant
+            transposition dans le Code de la propriété intellectuelle (CPI) de
+            la directive européenne du 11 mars 1996 relative à la protection
+            juridique des bases de données. Sont notamment interdites
+            l&pos;extraction et la réutilisation, quantitativement ou
+            qualitativement substantielles, du contenu des bases de données
+            contenues sur ce site Internet. Tout contrevenant s&pos;expose aux
+            sanctions visées aux articles L 343-1 et suivants du Code de la
+            propriété intellectuelle.
+          </p>
+          <p>
+            Les marques de XXX XXXX et de ses partenaires, ainsi que les logos
+            figurant sur le site sont des marques (semi-figuratives ou non) et
+            sont déposées. Toute reproduction totale ou partielle de ces marques
+            ou de ces logos effectuées à partir des éléments du site sans
+            l&pos;autorisation expresse XXX XXXX est prohibée, au sens des
+            articles L.713-2 et suivants du Code de la propriété intellectuelle.
+          </p>
+          <p>
+            Les utilisateurs visiteurs du site Internet ne peuvent mettre en
+            place un hyperlien en direction de ce site sans l&pos;autorisation
+            expresse et préalable de XXX XXXX. Nous remercions les utilisateurs
+            du site de nous faire part d&pos;éventuelles omissions, erreurs,
+            corrections, en remplissant le formulaire de contact.
+          </p>
+          <p></p>
+        </div>
+      </Section>
+    </main>
+  );
+};
+
+export default page;
