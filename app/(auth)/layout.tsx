27140af16e02e68fbab3f911e81b2d37d6432670
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-28 20:12:13 +0200

    remove auth folder

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="bg-slate-800 min-h-screen flex flex-col justify-center items-center">
-      {children}
-    </div>
-  )
-}
-
-export default Layout
