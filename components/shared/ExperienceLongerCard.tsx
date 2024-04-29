febe640508bc26ff61c8e2def2b2f3862d78b56a
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-29 12:52:32 +0200

    update css

--- components/shared/ExperienceLongerCard.tsx
+++ components/shared/ExperienceLongerCard.tsx
@@ -31,31 +31,40 @@ const ExperienceLongerCard = ({
       }  hover:border-accent`}
       onClick={onClick}
     >
+      {/* Img */}
       <Image src={src} alt={alt} className="rounded-xl" />
+
+      {/* Experience Name and Price */}
       <div className="flex flex-col gap-y-3 p-4">
         <div className="flex flex-row items-center justify-between">
-          <span className="cardTitle">{name}</span>
-          <span className="flex flex-row items-center gap-x-2">
-            dès{" "}
-            <span className="text-2xl font-semibold italic text-accent">
-              {" "}
+          <span className="text-xl font-semibold leading-6 md:text-3xl md:leading-7">
+            {name}
+          </span>
+          <span className="flex flex-row items-center gap-x-1 md:text-md xl:text-sm">
+            dès
+            <span className="text-xl font-semibold italic text-accent md:text-3xl ">
               {price}
-            </span>{" "}
-            / personnes{" "}
+            </span>
+            / personne
           </span>
         </div>
 
-        <span>{description}</span>
+        {/* Description */}
+        <span className="md:text-lg xl:text-md">{description}</span>
+
+        {/* People Number and Duration */}
         <div className="flex flex-row justify-between">
           <div className="flex flex-row items-center gap-x-1">
-            <User size={32} />
-            <span className="text-sm">{peopleNumber}</span>
+            <User className="cardIcon" />
+            <span className="text-sm md:text-md xl:text-sm">{peopleNumber}</span>
           </div>
           <div className="flex flex-row items-center gap-x-1">
-            <Hourglass size={32} />
-            <span className="text-sm">{duration}</span>
+            <Hourglass className="cardIcon" />
+            <span className="text-sm md:text-md xl:text-sm">{duration}</span>
           </div>
         </div>
+
+        {/* Selected */}
         <div className="absolute right-4 top-4 flex flex-row gap-x-1">
           {isSelected ? (
             <span className="rounded-xl bg-accent px-2 py-1">
