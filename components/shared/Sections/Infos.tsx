42e5b09d3e516bb56ac7d30ca05329024a50fb4f
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-29 16:41:54 +0200

    update infos component from md display to 2xl

--- components/shared/Sections/Infos.tsx
+++ components/shared/Sections/Infos.tsx
@@ -4,41 +4,42 @@ import SectionHeader from "../SectionHeader";
 
 const Infos = () => {
   return (
-    <Section marginBottom={true} marginTop={true} classname="">
+    <Section marginBottom={true} marginTop={true}>
       <SectionHeader title="Envie de tenter " titleHighlight="l'aventure ?" />
 
-      {/* When and Where  */}
-      <div className="mb-8">
-        <h3 className="text-balance">Quand et où nous trouver ?</h3>
+      <div className="flex flex-col gap-y-8 md:flex-row md:justify-center md:gap-x-20 md:gap-y-0 lg:gap-x-28">
+        {/* When and Where  */}
+        <div>
+          <h3 className="text-balance">Quand et où nous trouver ?</h3>
 
-        <div className="flex flex-col gap-y-4">
-          {/* Opening hours */}
-          <div className="flex flex-row gap-x-8  md:gap-y-2 ">
-            <div>
-              <Calendar size={48} color="#991b1b" />
+          <div className="flex flex-col gap-y-4">
+            {/* Opening hours */}
+            <div className="flex flex-row gap-x-8  md:gap-y-2 ">
+              <div>
+                <Calendar size={48} color="#991b1b" />
+              </div>
+              <div className="flex flex-col">
+                <span>7 jours / 7 </span>
+                <span>De 10h à 00h </span>
+              </div>
             </div>
-            <div className="flex flex-col">
-              <span>7 jours / 7 </span>
-              <span>De 10h à 00h </span>
-            </div>
-          </div>
 
-          {/* Address */}
-          <div className="flex flex-row gap-x-8  md:gap-y-2 ">
-            <div>
-              <LocateFixed size={48} color="#991b1b" />
-            </div>
-            <div className="flex flex-col">
-              <span>5 Rue Marcel Pagnol </span>
-              <span>13000, Marseille </span>
+            {/* Address */}
+            <div className="flex flex-row gap-x-8  md:gap-y-2 ">
+              <div>
+                <LocateFixed size={48} color="#991b1b" />
+              </div>
+              <div className="flex flex-col">
+                <span>5 Rue Marcel Pagnol </span>
+                <span>13000, Marseille </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Contact  */}
-      <div>
-        <h3>Nous contacter</h3>
+        {/* Contact  */}
+        <div>
+          <h3>Comment nous contacter ?</h3>
 
           <ul className="flex flex-col gap-y-4">
             {/* Phone */}
@@ -64,6 +65,7 @@ const Infos = () => {
             </li>
           </ul>
         </div>
+      </div>
     </Section>
   );
 };
