e56c0cf03e9dbf56f74875a0ec777d3cac7ac99d
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-29 13:43:08 +0200

    create PriceSection

---
+++ components/shared/Sections/Price.tsx
@@ -0,0 +1,17 @@
+import Section from "../Section";
+import SectionHeader from "../SectionHeader";
+
+const Price = () => {
+  return (
+    <Section marginBottom={true} marginTop={true} classname="">
+      <SectionHeader title="Des tarifs" titleHighlight="dégressifs" />
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
+    </Section>
+  );
+};
+
+export default Price;
