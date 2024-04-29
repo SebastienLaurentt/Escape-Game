ea40978806a3d7f0d829ef154bed482bcbc6b22e
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-29 16:58:11 +0200

    update css and add logo

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,13 +1,23 @@
 import { Mail, MapPin, Phone } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="px-6  md:px-10 xl:px-16 2xl:mx-auto 2xl:max-w-[2000px]">
+    <footer className="px-6  md:px-10 xl:mx-auto xl:max-w-[1600px] xl:px-16">
       <div className=" flex flex-col justify-between border-t border-slate-400 pt-4 text-xs  md:text-sm lg:py-8">
-        <span className="mb-6 text-2xl uppercase md:text-3xl xl:text-4xl">
-          La Villa de l&apos; <span className="text-accent">Effroi</span>
-        </span>
+        <div className="mb-6 flex flex-row items-center md:gap-x-4 lg:gap-x-8">
+          <span className=" text-2xl uppercase md:text-3xl xl:text-4xl">
+            La Villa de <span className="text-accent">l&apos; Effroi</span>
+          </span>
+          <Image
+            src="/images/logo.svg"
+            alt="Logo de la Villa de l'Effroi"
+            width={50}
+            height={50}
+          />
+        </div>
+
         <div className="gap-x-2 md:flex md:flex-row md:justify-between ">
           <div className="mb-6">
             <span className="text-lg uppercase text-slate-400">Services </span>
