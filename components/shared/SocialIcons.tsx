5ebf7c6473e75758fca24da1826264aa89e01cdf
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-30 19:28:49 +0200

    add fake instagram and facebook links

--- components/shared/SocialIcons.tsx
+++ components/shared/SocialIcons.tsx
@@ -10,12 +10,12 @@ const SocialIcons = ({id, classname}: SocialIconsPropos) => {
   return (
     <div id={id} className={`flex flex-row justify-center  ${classname}`}>
       <button aria-label="Allez à la page Instagram de la Villa de l'Effroi">
-        <Link href="" target="_blank">
+        <Link href="https://www.instagram.com/" target="_blank">
           <Instagram className="  size-8 md:size-10 xl:size-6  xl:hover:text-accent" />
         </Link>
       </button>
       <button aria-label="Allez à la page Facebook de la Villa de l'Effroi">
-        <Link href="" target="_blank">
+        <Link href="https://www.facebook.com/" target="_blank">
           <Facebook className="size-8 md:size-10 xl:size-6 xl:hover:text-accent" />
         </Link>
       </button>
