30d69dea0270a2b320c48025b87385b5acaa6eaf
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-30 19:13:18 +0200

    update data file with aria labels

--- data/navData.ts
+++ data/navData.ts
@@ -1,16 +1,21 @@
+import { ar } from "date-fns/locale";
+
 const navData = [
   {
     href: "/",
+    ariaLabel: "Aller à la page d'accueil",
     name: "Accueil",
     burgerMenuName: "ACCUEIL",
   },
   {
     href: "/experiences",
+    ariaLabel: "Aller à la page pour réserver une expérience",
     name: "Expériences",
     burgerMenuName: "EXPERIENCES",
   },
   {
     href: "/contact",
+    ariaLabel: "Aller à la page de contact",
     name: "Contact",
     burgerMenuName: "CONTACT",
   },
