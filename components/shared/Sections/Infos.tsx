b0ca79fffed9d8bb1f813215bc0e07eeed79f411
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-30 19:49:42 +0200

    update content and css

--- components/shared/Sections/Infos.tsx
+++ components/shared/Sections/Infos.tsx
@@ -11,8 +11,10 @@ const Infos = () => {
 
       <div className="flex flex-col gap-y-8 md:flex-row md:justify-center md:gap-x-20 md:gap-y-0 lg:gap-x-28">
         {/* When and Where  */}
-        <div>
-          <h3 className="text-balance">Quand et où nous trouver ?</h3>
+        <div className="flex flex-col items-center">
+          <h3 className="w-[255px] text-center md:text-left">
+            Quand et où nous trouver ?
+          </h3>
 
           <div className="flex flex-col gap-y-4">
             {/* Opening hours */}
@@ -40,32 +42,30 @@ const Infos = () => {
         </div>
 
         {/* Contact  */}
-        <div>
-          <h3>Comment nous contacter ?</h3>
+        <div className="flex flex-col items-center">
+          <h3 className="w-[255px] text-center md:text-left">
+            Comment nous contacter ?
+          </h3>
 
-          <ul className="flex flex-col gap-y-4">
+          <div className="flex flex-col gap-y-4">
             {/* Phone */}
-            <li>
-              <div className="flex flex-row gap-x-8  md:gap-y-2 ">
-                <Phone size={48} color="#991b1b" />
-                <div className="flex flex-col">
-                  <span>Téléphone</span>
-                  <span>06 12 34 56 78</span>
-                </div>
+            <div className="flex flex-row  gap-x-8  md:gap-y-2 ">
+              <Phone size={48} color="#991b1b" />
+              <div className="flex flex-col">
+                <span>Téléphone</span>
+                <span>06 12 34 56 78</span>
               </div>
-            </li>
+            </div>
 
             {/* Email */}
-            <li>
-              <div className="flex flex-row  gap-x-8 md:gap-y-2">
-                <Mail size={48} color="#991b1b" />
-                <div className="flex flex-col">
-                  <span>Email</span>
-                  <span>lavilladeleffroi@gmail.com</span>
-                </div>
+            <div className="flex flex-row  gap-x-8 md:gap-y-2">
+              <Mail size={48} color="#991b1b" />
+              <div className="flex flex-col">
+                <span>Email</span>
+                <span>villaeffroi@gmail.com</span>
               </div>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </div>
       <div className="my-8 flex flex-row justify-center lg:my-12">
