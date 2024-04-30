e9d53aadb260d04134a002c51d71a2e2fbde4946
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-30 21:51:58 +0200

    day outside are invisible & without hover

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -51,15 +51,15 @@ function Calendar({
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "hover:bg-accent rounded-md h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: " rounded-md h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/0 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: 
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-md",
+          "hover:bg-accent h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-md",
         day_range_end: "day-range-end",
         day_selected:
           "bg-accent text-foreground hover:bg-primary hover:text-primary-foreground focus:bg-accent focus:text-foreground",
         day_today: "bg-primary text-primary-foreground",
         day_outside:
-          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
+          "day-outside invisible text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-100",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
