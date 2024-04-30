6c825efd56a33f54be394497c9ebc626a330f199
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-30 16:51:00 +0200

    remove one card price and update other content

--- components/shared/Sections/Price.tsx
+++ components/shared/Sections/Price.tsx
@@ -9,18 +9,15 @@ const Price = () => {
       <ul className="flex flex-col items-center justify-center gap-y-4 md:gap-y-6 xl:flex-row xl:gap-x-6">
         <div className="flex flex-col  gap-y-4 md:flex-row md:gap-x-6 ">
           <li>
-            <PriceCard peopleNumber="1 à 2 personnes" price="40€" />
+            <PriceCard peopleNumber="2 personnes" price="35€" />
           </li>
           <li>
-            <PriceCard peopleNumber="3 à 4 personnes" price="35€" />
+            <PriceCard peopleNumber="3 à 4 personnes" price="30€" />
           </li>
         </div>
         <div className="flex flex-col gap-y-4 md:flex-row md:gap-x-6 ">
           <li>
-            <PriceCard peopleNumber="5 à 6 personnes" price="30€" />
-          </li>
-          <li>
-            <PriceCard peopleNumber="6 à 7 personnes" price="25€" />
+            <PriceCard peopleNumber="5 à 6 personnes" price="25€" />
           </li>
         </div>
       </ul>
