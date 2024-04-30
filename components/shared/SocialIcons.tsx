e40f43cd89a6380246a8baef6514302582eda02d
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-30 19:07:47 +0200

    update ariaLabel

--- components/shared/SocialIcons.tsx
+++ components/shared/SocialIcons.tsx
@@ -9,12 +9,12 @@ interface SocialIconsPropos {
 const SocialIcons = ({id, classname}: SocialIconsPropos) => {
   return (
     <div id={id} className={`flex flex-row justify-center  ${classname}`}>
-      <button aria-label="Allez à la page Instagram de Escape Room">
+      <button aria-label="Allez à la page Instagram de la Villa de l'Effroi">
         <Link href="" target="_blank">
           <Instagram className="  size-8 md:size-10 xl:size-6  xl:hover:text-accent" />
         </Link>
       </button>
-      <button aria-label="Allez à la page Facebook de Escape Room">
+      <button aria-label="Allez à la page Facebook de la Villa de l'Effroi">
         <Link href="" target="_blank">
           <Facebook className="size-8 md:size-10 xl:size-6 xl:hover:text-accent" />
         </Link>
