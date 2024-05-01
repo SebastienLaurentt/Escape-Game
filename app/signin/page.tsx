6f84d35be4d34b17ba9bcba16cd6032e6c94747e
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-01 22:07:37 +0200

    update content and css of signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,15 @@
-import SignInButton from '@/components/shared/SignInButton'
+import SignInButton from "@/components/shared/SignInButton";
 
 const page = () => {
   return (
-    <div><SignInButton/></div>
-  )
-}
+    <div className="flex h-screen flex-row items-center justify-center">
+      <div className="flex flex-col items-center gap-y-4 rounded-xl bg-slate-800 py-12">
+        <h2 className="text-center uppercase">La Villa de l&apos;effroi</h2>
+        <h3>Espace Administrateur</h3>
+        <SignInButton />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
