3efd4a4bfd2ffed90e37e6f428c82ba2962e8e71
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-02 11:47:09 +0200

    update content and css

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,8 +1,9 @@
+import Section from "@/components/shared/Section";
 import SignInButton from "@/components/shared/SignInButton";
 import { auth } from "@/src/auth/auth";
 import { redirect } from "next/navigation";
 
-export default async function SignIn ()  {
+export default async function SignIn() {
   const session = await auth();
 
   // If the user is not authenticated, redirect to the sign-in page
@@ -11,15 +12,17 @@ export default async function SignIn ()  {
   }
 
   return (
-    <div className="flex h-screen flex-row items-center justify-center">
-      <div className="flex flex-col items-center gap-y-4 rounded-xl bg-slate-800 py-12">
-        <h2 className="text-center uppercase">La Villa de l&apos;effroi</h2>
-        <h3>Espace Administrateur</h3>
-        <SignInButton />
-      </div>
-    </div>
+    <main className="flex h-screen flex-row items-center justify-center">
+      <Section>
+        <div className="flex flex-col items-center gap-y-4 rounded-xl bg-slate-900 py-12">
+          <h2 className="flex flex-col text-center uppercase">
+            <span>La Villa de</span>
+            <span className="text-accent">l&apos;effroi</span>
+          </h2>
+          <h3>Espace Administrateur</h3>
+          <SignInButton />
+        </div>
+      </Section>
+    </main>
   );
-};
-
-
-
+}
