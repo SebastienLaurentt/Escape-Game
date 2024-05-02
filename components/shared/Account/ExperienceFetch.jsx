b6d68399d2486cba98c14854c790d91de6d445c9
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-03 01:01:29 +0200

    experience data fetch working

---
+++ components/shared/Account/ExperienceFetch.jsx
@@ -0,0 +1,27 @@
+import prisma from "@/lib/client";
+
+async function getExperiences() {
+  const experiences = await prisma.experience.findMany();
+  return experiences;
+}
+
+export default async function ExperienceFetch() {
+  const experiences = await getExperiences();
+  console.log(experiences);
+  return (
+    <div>
+      <ul>
+        {experiences.map((experience) => (
+          <li key={experience.id}>
+            <div>
+              <h2>{experience.name}</h2>
+              <p>{experience.description}</p>
+              <span> Nombre de personnes: {experience.minPeople} à {experience.maxPeople} personnes  </span>
+              <span> Durée : {experience.duration} {experience.durationUnit}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
