8d4eb073a1fe7f458bc1e5fd90287eb6f62b8972
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-02 14:27:55 +0200

    update signIn page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Section from "@/components/shared/Section";
+import SignInButton from "@/components/shared/SignInButton";
+import { auth } from "@/src/auth/auth";
+import { redirect } from "next/navigation";
 
-const page = () => {
+export default async function SignIn() {
+  const session = await auth();
+
+  // If the user is not authenticated, redirect to the sign-in page
+  if (session) {
+    redirect("/account");
+  }
+
   return (
-    <div>Admin</div>
-  )
+    <main className="flex h-screen flex-row items-center justify-center">
+      <Section>
+        <div className="flex flex-col items-center gap-y-4 rounded-xl bg-slate-900 p-12 lg:p-20">
+          <h2 className="flex flex-col text-center uppercase">
+            <span>La Villa de</span>
+            <span className="text-accent">l&apos;effroi</span>
+          </h2>
+          <h3>Espace Administrateur</h3>
+          <SignInButton />
+        </div>
+      </Section>
+    </main>
+  );
 }
-
-export default page
