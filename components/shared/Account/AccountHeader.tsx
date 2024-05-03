d39ca1d9ec23f26b20e276c97c5b217f7a790173
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-03 16:45:52 +0200

    create accountHeader component

---
+++ components/shared/Account/AccountHeader.tsx
@@ -0,0 +1,15 @@
+import { Link } from "lucide-react";
+import SignOutButton from "../SignOutButton";
+
+const AccountHeader = () => {
+  return (
+    <header className="w-full">
+      <div className="flex flex-row items-center  justify-between  px-6 py-4 lg:px-10 lg:py-6 xl:mx-auto xl:px-16 2xl:max-w-[2000px]">
+        <Link href="/account">Dashboard</Link>
+        <SignOutButton />
+      </div>
+    </header>
+  );
+};
+
+export default AccountHeader;
