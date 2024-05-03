fb80fafdaa93a3af054cc587453b0fc4c283ca5d
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-03 12:16:10 +0200

    create action to type experience schema with zod & to prepare prisma queries

--- components/shared/Account/ExperienceFetch.tsx
+++ components/shared/Account/ExperienceFetch.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-
+import Link from "next/link";
 
 async function getExperiences() {
   const experiences = await prisma.experience.findMany();
@@ -8,21 +8,13 @@ async function getExperiences() {
 
 export default async function ExperienceFetch() {
   const experiences = await getExperiences();
-  console.log(experiences);
   return (
-    <div>
-      <ul>
-        {experiences.map((experience) => (
-          <li key={experience.id}>
-            <div>
-              <h2>{experience.name}</h2>
-              <p>{experience.description}</p>
-              <span> Nombre de personnes: {experience.minPeople} à {experience.maxPeople} personnes  </span>
-              <span> Durée : {experience.duration} {experience.durationUnit}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="flex flex-row gap-x-4">
+      {experiences.map((experience) => (
+        <li key={experience.id} className="rounded-lg bg-background px-3 py-1">
+          <Link href={`/account/${experience.name}`}>{experience.name}</Link>
+        </li>
+      ))}
+    </ul>
   );
 }
