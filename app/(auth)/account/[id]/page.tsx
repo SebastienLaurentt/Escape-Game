110b2c009e57c898c80597050d5d51e76140bc22
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-03 15:31:39 +0200

    update css

--- app/(auth)/account/[id]/page.tsx
+++ app/(auth)/account/[id]/page.tsx
@@ -1,22 +1,22 @@
-
 import UpdateForm from "@/components/shared/Account/UpdateExperience";
+import Section from "@/components/shared/Section";
 import { getExperienceById } from "@/lib/action";
 import { notFound } from "next/navigation";
 
 const UpdateExperiencePage = async ({ params }: { params: { id: string } }) => {
   const id = params.id;
   const experience = await getExperienceById(id);
-  console.log(experience)
+  console.log(experience);
 
   if (!experience) {
     notFound();
   }
 
   return (
-    <div className="mx-auto mt-5 max-w-md">
-      <h1 className="mb-2 text-center text-2xl">Update Experience</h1>
+    <Section classname="my-8">
+      <h1 className="mb-8 text-center">Update Experience</h1>
       <UpdateForm experience={experience} />
-    </div>
+    </Section>
   );
 };
 
