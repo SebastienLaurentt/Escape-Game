5aaa87edff41ac515acd18a0ad30af6c1d0fa9c2
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-18 19:25:22 +0200

    reformat date

--- components/shared/Account/OpenDays.tsx
+++ components/shared/Account/OpenDays.tsx
@@ -2,12 +2,24 @@ import { ClosedDay } from "@prisma/client";
 import { DeleteButton } from "./DeleteClosedDay";
 
 const OpenDays = ({ closedDays }: { closedDays: ClosedDay[] }) => {
+  const formatDate = (date: Date) => {
+    const formattedDate = new Intl.DateTimeFormat("fr-FR", {
+      weekday: "long",
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }).format(date);
+
+    // Uppercase to Weekday
+    return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+  };
+
   return (
     <div>
       {closedDays.map((day) => (
-        <div key={day.id} className="flex flex-rwo items-center gap-x-4">
-          <span>{day.date.toISOString()}</span>{" "}
-          <DeleteButton id={day.id}/> 
+        <div key={day.id} className="flex-rwo flex items-center gap-x-4">
+          <span>{formatDate(new Date(day.date))}</span>{" "}
+          <DeleteButton id={day.id} />
         </div>
       ))}
     </div>
