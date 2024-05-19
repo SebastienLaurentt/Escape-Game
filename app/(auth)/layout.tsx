b5574d959f09798b901e058c7bb4e8f75cc72537
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-19 15:45:08 +0200

    create MobileNav component for accountLayout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from "@/components/shared/Account/MobileNav";
 import TabDeskNav from "@/components/shared/Account/TabDeskNav";
 import SignOutButton from "@/components/shared/SignOutButton";
 import { Button } from "@/components/ui/button";
@@ -31,52 +32,7 @@ export default function RootLayout({
 
         {/* Toggle Menu : Only Mobile ?  */}
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-          <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button size="icon" variant="outline" className="sm:hidden">
-                  <PanelLeft className="size-5" />
-                  <span className="sr-only">Toggle Menu</span>
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="left" className="sm:max-w-xs">
-                <nav className="grid gap-6 text-lg font-medium">
-                  <Link
-                    href="#"
-                    className="group flex size-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-                  >
-                    <LayoutDashboard className="size-5 transition-all group-hover:scale-110" />
-                    <span className="sr-only">Dashboard Logo</span>
-                  </Link>
-                  <Link
-                    href="/account/experiences/"
-                    className="flex items-center gap-4 px-2.5 text-foreground"
-                  >
-                    <Gamepad2 className="size-5" />
-                    Expériences
-                  </Link>
-                  <Link
-                    href="/account/opening/"
-                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                  >
-                    <Calendar className="size-5" />
-                    Ouverture
-                  </Link>
-                  <Link
-                    href="/account/reservations/"
-                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                  >
-                    <Users2 className="size-5" />
-                    Réservations
-                  </Link>
-                </nav>
-              </SheetContent>
-            </Sheet>
-
-            <div className="ml-auto">
-              <SignOutButton />
-            </div>
-          </header>
+          <MobileNav />
 
           {/* Main */}
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 xl:grid-cols-3">
