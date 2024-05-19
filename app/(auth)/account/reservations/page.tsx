a76d414c83ea629969da94116cc4f89891b8548e
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-19 11:41:24 +0200

    create reservation page

---
+++ app/(auth)/account/reservations/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@/src/auth/auth";
+import { redirect } from "next/navigation";
+
+export default async function Reservations() {
+  const session = await auth();
+
+  // If the user is not authenticated, redirect to the signIn page
+  if (!session) {
+    redirect("/signin");
+  }
+
+  return (
+    <>
+      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2"></div>
+    </>
+  );
+}
