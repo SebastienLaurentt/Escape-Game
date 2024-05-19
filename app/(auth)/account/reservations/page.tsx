0de406013ce43f20aecbcc431762e4d12792fed3
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-19 12:15:28 +0200

    create reservations header

--- app/(auth)/account/reservations/page.tsx
+++ app/(auth)/account/reservations/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/src/auth/auth";
 import { redirect } from "next/navigation";
+import ReservationsHeader from "./ReservationsHeader";
 
 export default async function Reservations() {
   const session = await auth();
@@ -11,7 +12,9 @@ export default async function Reservations() {
 
   return (
     <>
-      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2"></div>
+      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+        <ReservationsHeader />
+      </div>
     </>
   );
 }
