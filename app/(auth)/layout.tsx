dc40a9effe0a841d4d0709e370f579c9cade6314
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-19 11:51:06 +0200

    replace dashboard logo link with span

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -38,13 +38,13 @@ export default function RootLayout({
         {/* Left Aside : Tablette and Desktop */}
         <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
           <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-            <Link
+            <span
               href="#"
               className="group flex size-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:size-8 md:text-base"
             >
               <LayoutDashboard className="size-4" />
               <span className="sr-only">Dashboard Logo</span>
-            </Link>
+            </span>
             <TooltipProvider>
               <Tooltip>
                 <TooltipTrigger asChild>
