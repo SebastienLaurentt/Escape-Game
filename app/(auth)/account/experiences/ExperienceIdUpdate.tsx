95e88e776576982b28e07dc78e749227e6313208
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-23 20:13:43 +0200

    update css

--- app/(auth)/account/experiences/ExperienceIdUpdate.tsx
+++ app/(auth)/account/experiences/ExperienceIdUpdate.tsx
@@ -207,7 +207,9 @@ const ExperienceIdUpdate = ({ experience }: { experience: Experience }) => {
             <div className="flex flex-col gap-y-2 md:flex-row md:items-center md:gap-x-4 md:gap-y-0">
               <Button>Update</Button>
               <div id="message-error" aria-live="polite" aria-atomic="true">
-                <p className="text-base text-center text-green-600">{state?.message}</p>
+                <p className="text-center text-base text-green-600">
+                  {state?.message}
+                </p>
               </div>
             </div>
           </form>
