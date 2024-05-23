297f3f92b452c438bcba69167a40f0281e88fe8d
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-23 23:51:45 +0200

    remove dynamic import

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,8 @@
-import dynamic from 'next/dynamic';
 
 import Hero from "@/components/shared/Sections/Hero";
 import Infos from "@/components/shared/Sections/Infos";
 import Price from "@/components/shared/Sections/Price";
-const Experiences = dynamic(() => import("../../components/shared/Sections/Experiences"), {
-  ssr: true,
-});
+import Experiences from "../../components/shared/Sections/Experiences";
 
 export default function Home() {
   return (
