38598b8e1a887e0804c6c2d75826fec3106f09a0
SebastienLaurentt <slaurent.26@gmail.com>
2024-05-24 12:06:59 +0200

    use noStore in experienceFlow

--- app/(root)/experiences/page.tsx
+++ app/(root)/experiences/page.tsx
@@ -1,8 +1,10 @@
 import PageTitle from "@/components/shared/PageTitle";
 import ReservationFlow from "@/components/shared/ReservationFlow";
 import { getClosedDay, getExperiencesList } from "@/lib/action";
+import { unstable_noStore } from "next/cache";
 
 const Experiences = async () => {
+  unstable_noStore();
   const closedDays = await getClosedDay();
   const experiences = await getExperiencesList();
   return (
