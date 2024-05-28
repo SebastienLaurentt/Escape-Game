da82c021c133951604dcf66ebe98389fd090101e
Sebastien Laurent <slaurent.26@gmail.com>
2024-05-28 13:07:55 +0200

    update from email

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -10,15 +10,10 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: Request) {
   try {
-    console.log('Starting POST request handler')
     const body = await req.text()
     const signature = headers().get('stripe-signature')
 
-    console.log('Received body:', body)
-    console.log('Received signature:', signature)
-
     if (!signature) {
-      console.log('Missing signature')
       return new Response('Invalid signature', { status: 400 })
     }
 
@@ -28,23 +23,23 @@ export async function POST(req: Request) {
       process.env.STRIPE_WEBHOOK_SECRET!
     )
 
-    console.log('Constructed Stripe event:', event)
-
     if (event.type === 'checkout.session.completed') {
-      console.log('Checkout session completed event received')
       if (!event.data.object.customer_details?.email) {
         throw new Error('Missing user email')
       }
 
       const session = event.data.object as Stripe.Checkout.Session
 
-      const { orderId } = session.metadata || { orderId: null }
+      const {  orderId } = session.metadata || {
+        orderId: null,
+      }
 
-      console.log('Order ID from session metadata:', orderId)
-
-      if (!orderId) {
+      if ( !orderId) {
         throw new Error('Invalid request metadata')
       }
+
+      // const billingAddress = session.customer_details!.address
+      // const shippingAddress = session.shipping_details!.address
 
       const updatedOrder = await prisma.order.update({
         where: {
@@ -52,29 +47,40 @@ export async function POST(req: Request) {
         },
         data: {
           isPaid: true,
+          // shippingAddress: {
+          //   create: {
+          //     name: session.customer_details!.name!,
+          //     city: shippingAddress!.city!,
+          //     country: shippingAddress!.country!,
+          //     postalCode: shippingAddress!.postal_code!,
+          //     street: shippingAddress!.line1!,
+          //     state: shippingAddress!.state,
+          //   },
+          // },
+          // billingAddress: {
+          //   create: {
+          //     name: session.customer_details!.name!,
+          //     city: billingAddress!.city!,
+          //     country: billingAddress!.country!,
+          //     postalCode: billingAddress!.postal_code!,
+          //     street: billingAddress!.line1!,
+          //     state: billingAddress!.state,
+          //   },
+          // },
         },
       })
 
-      console.log('Order updated successfully', updatedOrder)
-
-      try {
-        const emailResponse = await resend.emails.send({
-          from: 'VillaEffroi <lavillaeffroi@gmail.com>',
-          to: 'slaurent.26@gmail.com',
-          subject: 'Thanks for your order!',
-          react: BookingReceivedEmail(),
-        })
-
-        console.log('Email sent successfully', emailResponse)
-      } catch (emailError) {
-        console.error('Failed to send email', emailError)
-        throw new Error('Email sending failed')
-      }
+      await resend.emails.send({
+        from: 'VillaEffroi <noreply@villaeffroi.info>',
+        to: 'slaurent.26@gmail.com',
+        subject: 'Thanks for your order!',
+        react: BookingReceivedEmail(),
+      })
     }
 
     return NextResponse.json({ result: event, ok: true })
   } catch (err) {
-    console.error('Error handling POST request:', err)
+    console.error(err)
 
     return NextResponse.json(
       { message: 'Something went wrong', ok: false },
