cf125c27ea6d9233f4223b31dceb43092a123308
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-04 12:30:46 +0200

    set useRef in ul

--- components/shared/Sections/Experiences.tsx
+++ components/shared/Sections/Experiences.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { Button } from "@/components/ui/button";
 import { Experience } from "@prisma/client";
@@ -11,12 +11,12 @@ import SectionHeader from "../SectionHeader";
 
 const Experiences = ({ experiences }: { experiences: Experience[] }) => {
   const sectionHeaderRef = useRef<HTMLDivElement | null>(null);
-  const contentRef = useRef<HTMLDivElement | null>(null);
+  const listRef = useRef<HTMLUListElement | null>(null);
   const isSectionHeaderInView = useInView(sectionHeaderRef, {
     once: true,
     amount: 0.4,
   });
-  const isContentInView = useInView(contentRef, { once: true, amount: 0.4 });
+  const isListInView = useInView(listRef, { once: true, amount: 0.4 });
 
   return (
     <Section marginBottom={true} marginTop={true} classname="">
@@ -26,43 +26,41 @@ const Experiences = ({ experiences }: { experiences: Experience[] }) => {
           isSectionHeaderInView ? "opacity-100" : ""
         }`}
       >
-        <SectionHeader title="Des expériences" titleHighlight="effrayantes" />
+        <SectionHeader title="Des expériences" titleHighlight="variées" />
       </div>
 
-      <div
-        ref={contentRef}
+      {/* Experience Card List */}
+      <ul
+        ref={listRef}
         className={`opacity-0 transition-opacity delay-300 duration-700 ease-in-out ${
-          isContentInView ? "opacity-100" : ""
-        }`}
+          isListInView ? "opacity-100" : ""
+        } flex flex-col gap-y-8 md:px-20 lg:px-40 xl:flex-row xl:justify-center xl:gap-x-2 xl:px-0 2xl:gap-x-4 2xl:px-12`}
       >
-        {/* Experience Card List */}
-        <ul className="flex flex-col gap-y-8 md:px-20 lg:px-40 xl:flex-row xl:justify-center xl:gap-x-2 xl:px-0 2xl:gap-x-4 2xl:px-12">
-          {experiences.map((experience: Experience, index) => (
-            <li key={index}>
-              <ExperienceCard
-                src={`https://igppurftciumtqmwijea.supabase.co/storage/v1/object/public/images/${experience.image}`}
-                name={experience.name}
-                minPrice={experience.minPrice}
-                description={experience.description}
-                minPeople={experience.minPeople}
-                maxPeople={experience.maxPeople || ""}
-                duration={experience.duration}
-                durationUnit={experience.durationUnit}
-                hover={false}
-              />
-            </li>
-          ))}
-        </ul>
+        {experiences.map((experience: Experience, index) => (
+          <li key={index}>
+            <ExperienceCard
+              src={`https://igppurftciumtqmwijea.supabase.co/storage/v1/object/public/images/${experience.image}`}
+              name={experience.name}
+              minPrice={experience.minPrice}
+              description={experience.description}
+              minPeople={experience.minPeople}
+              maxPeople={experience.maxPeople || ""}
+              duration={experience.duration}
+              durationUnit={experience.durationUnit}
+              hover={false}
+            />
+          </li>
+        ))}
+      </ul>
 
-        {/* Link Button to Experience / Reservation page */}
-        <div className="my-8 flex flex-row justify-center lg:my-12">
-          <Button
-            asChild
-            aria-label="Aller à la page pour réserver son expérience"
-          >
-            <Link href="/reservation/experiences">Réserver</Link>
-          </Button>
-        </div>
+      {/* Link Button to Experience / Reservation page */}
+      <div className="my-8 flex flex-row justify-center lg:my-12">
+        <Button
+          asChild
+          aria-label="Aller à la page pour réserver son expérience"
+        >
+          <Link href="/reservation/experiences">Réserver</Link>
+        </Button>
       </div>
     </Section>
   );
