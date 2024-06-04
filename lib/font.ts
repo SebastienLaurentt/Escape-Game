8113e741d43cef67b2031cd79228022c2f3bcf74
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-04 10:56:23 +0200

    set poppins font in font file

--- lib/font.ts
+++ lib/font.ts
@@ -1,5 +1,13 @@
-import { Creepster } from "next/font/google";
+import { Creepster, Poppins } from "next/font/google";
+
+const poppins_init = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 const creepster_init = Creepster({ subsets: ["latin"], weight: "400", variable: "--font-creepster",});
+
+export const poppins = poppins_init.className;
 
 export const creepster = creepster_init.className;
