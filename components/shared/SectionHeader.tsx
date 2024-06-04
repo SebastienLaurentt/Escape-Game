02f9dcb4416b02298d7db208251c7860cd7d1fc7
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-04 11:13:19 +0200

    add creepster font to section header

--- components/shared/SectionHeader.tsx
+++ components/shared/SectionHeader.tsx
@@ -1,3 +1,4 @@
+import { creepster } from "@/lib/font";
 import React from "react";
 
 interface SectionHeaderProps {
@@ -12,8 +13,10 @@ const SectionHeader: React.FC<SectionHeaderProps> = ({
   titleDescription,
 }) => {
   return (
-    <div className="mb-12 flex flex-col items-center text-center md:mb-16 ">
-      <h2 className="flex flex-col">
+    <div className="mb-6 mt-4 flex flex-col items-center text-center md:mb-8">
+      <h2
+        className={`${creepster} flex flex-col text-3xl leading-[44px] md:text-5xl md:leading-[56px] lg:text-6xl lg:leading-[64px]`}
+      >
         <span>{title}</span>{" "}
         <span className="text-primary">{titleHighlight}</span>
       </h2>
