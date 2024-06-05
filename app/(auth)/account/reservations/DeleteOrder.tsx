3d11173f27481ac1c4c01673e72d032c0967b8b6
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-05 23:05:10 +0200

    replace DeleteReservationWithId with DeleteOrderWithId in DeleteReservation component (now DeleteOrder)

--- app/(auth)/account/reservations/DeleteOrder.tsx
+++ app/(auth)/account/reservations/DeleteOrder.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { deleteReservation } from "@/lib/action";
+import { deleteOrder } from "@/lib/action";
 import { Trash } from "lucide-react";
 
 export const DeleteOrder = ({ id }: { id: string }) => {
-  const DeleteReservationWithId = deleteReservation.bind(null, id);
+  const DeleteOrderWithId = deleteOrder.bind(null, id);
   return (
-    <form action={DeleteReservationWithId}>
+    <form action={DeleteOrderWithId}>
       <Button size="sm">
         <Trash />
       </Button>
