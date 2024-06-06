126afb4f7cb52f510d08f6dbce1634784123c515
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-06 17:32:47 +0200

    remove icon type (building issue)

--- components/shared/Account/MobileNav.tsx
+++ components/shared/Account/MobileNav.tsx
@@ -16,7 +16,7 @@ const MobileNav = () => {
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <Sheet>
         <SheetTrigger asChild>
-          <Button size="icon" variant="outline" className="sm:hidden">
+          <Button variant="outline" className="sm:hidden">
             <PanelLeft className="size-5" />
             <span className="sr-only">Toggle Menu</span>
           </Button>
