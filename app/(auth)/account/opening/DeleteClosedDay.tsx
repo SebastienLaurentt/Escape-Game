694581e4f7c601ea3eca5182a4f913678e9d87c7
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-08 22:13:04 +0200

    use react query to deleteClosedDay

--- app/(auth)/account/opening/DeleteClosedDay.tsx
+++ app/(auth)/account/opening/DeleteClosedDay.tsx
@@ -1,12 +1,41 @@
-import { Button } from "@/components/ui/button";
-import { deleteClosedDay } from "@/lib/action";
+'use client'
 
+import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { deleteClosedDay } from "@/lib/action";
+import { toast } from "@/components/ui/use-toast";
+import { Trash } from "lucide-react";
+import Loader from "@/components/shared/Loader";
 
 export const DeleteButton = ({ id }: { id: string }) => {
-  const DeleteClosedDaytWithId = deleteClosedDay.bind(null, id);
+  const { mutate: deleteClosedDayMutation, isPending } = useMutation({
+    mutationKey: ["delete-closed-day"],
+    mutationFn: () => deleteClosedDay(id),
+    onSuccess: () => {
+      toast({
+        title: "Closed day reopened successfully",
+        variant: "destructive",
+      });
+    },
+    onError: (error: any) => {
+      toast({
+        title: "Failed to reopen closed day",
+        description: error.message,
+        variant: "destructive",
+      });
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    deleteClosedDayMutation();
+  };
+
   return (
-    <form action={DeleteClosedDaytWithId}>
-      <Button>Réouvrir</Button>
+    <form onSubmit={handleSubmit}>
+      <Button type="submit" disabled={isPending}>
+        {isPending ? <Loader /> : "Réouvrir"}
+      </Button>
     </form>
   );
 };
