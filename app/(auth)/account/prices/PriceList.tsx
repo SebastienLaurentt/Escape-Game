dab0a9994b6948e9224c8701cd318daea3e2a29e
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-11 02:19:19 +0200

    create priceList component

---
+++ app/(auth)/account/prices/PriceList.tsx
@@ -0,0 +1,29 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { getPricesList } from "@/lib/action";
+
+const PriceList = async () => {
+  const priceList = await getPricesList();
+  return (
+    <Tabs defaultValue="week">
+      <TabsContent value="week">
+        <Card>
+          <CardContent className="flex flex-col gap-4 p-6 ">
+            <div className="grid grid-cols-2 font-bold lg:grid-cols-4">
+              <div className="mx-auto w-[100px]">Nombre de personnes</div>
+              <div className="my-auto text-center">Prix</div>
+            </div>
+            {priceList.map(({ people, price }, index) => (
+              <div key={index} className="grid grid-cols-2 lg:grid-cols-4">
+                <div className="text-center">{people}</div>
+                <div className="text-center">{price}</div>
+              </div>
+            ))}
+          </CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+export default PriceList;
--- app/(auth)/account/prices/page.tsx
+++ app/(auth)/account/prices/page.tsx
@@ -2,6 +2,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 import { redirect } from "next/navigation";
 import PriceHeader from "./PriceHeader";
+import PriceList from "./PriceList";
 
 export default async function Opening() {
   const { isAuthenticated } = getKindeServerSession();
@@ -12,6 +13,7 @@ export default async function Opening() {
     <>
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
         <PriceHeader />
+        <PriceList />
       </div>
     </>
   );
