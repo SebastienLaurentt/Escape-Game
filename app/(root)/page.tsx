72bf25bd486fe8b349ba149651fdebfefef6bb5c
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-12 12:03:46 +0200

    remove price section on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
 import Hero from "@/components/shared/Sections/Hero";
 import Infos from "@/components/shared/Sections/Infos";
-import Price from "@/components/shared/Sections/Price";
 import { Testimonials } from "@/components/shared/Sections/Testimonials";
-import Experiences from "../../components/shared/Sections/Experiences";
 import { getExperiencesList } from "@/lib/action";
 import { unstable_noStore } from "next/cache";
+import Experiences from "../../components/shared/Sections/Experiences";
 
 const Home = async () => {
   unstable_noStore();
@@ -13,12 +12,10 @@ const Home = async () => {
     <main>
       <Hero />
       <Experiences experiences={experiences} />
-      <Price />
       <Testimonials />
       <Infos />
-      
     </main>
   );
-}
+};
 
 export default Home;
