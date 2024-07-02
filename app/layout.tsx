475baf064e6598e81e9f1fe16b2b129378293373
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-02 19:14:08 +0200

    add og and twitter img

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "La Villa de l'Effroi",
   description: "Bienvenue à la Villa de l'Effroi, l'escape game de l'horreur !",
-  metadataBase: new URL('https://escape-game-pi.vercel.app/'),
 };
 
 export default function RootLayout({
